67bb12f6c1e4b4a34e83bcfc167b09f6eb005c70
Alex Willemsma <alex@undergroundwebdevelopment.com>
2015-01-19 19:52:45 -0800

    Fix broken migration, and add user relation to offers frontend form.

--- db/migrate/20150120034420_add_user_id_to_offers.rb
+++ db/migrate/20150120034420_add_user_id_to_offers.rb
@@ -1,8 +1,9 @@
 class AddUserIdToOffers < ActiveRecord::Migration
   def change
     change_table :offers do |t|
-      t.integer :user_id, null: false
-      t.add_foreign_key :offers, :users
+      t.integer :user_id, null: false, after: :id
     end
+
+    add_foreign_key :offers, :users
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150120032130) do
+ActiveRecord::Schema.define(version: 20150120034420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150120032130) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id",     null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +43,5 @@ ActiveRecord::Schema.define(version: 20150120032130) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "offers", "users"
 end
