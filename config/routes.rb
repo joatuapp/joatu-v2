19454dd1e3d37af3078c78616508a0659abba6d5
Alex Willemsma <alex@undergroundwebdevelopment.com>
2015-01-31 18:05:28 -0800

    Add redirects to blog links, prep for pointing joatu.com at the app.

--- config/routes.rb
+++ config/routes.rb
@@ -82,4 +82,20 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  
+  # These are a bunch of redirect routes for old blog articles, so that when we
+  # point joatu.com at this app we don't break the old links.
+  get '/faq', to: redirect("https://joatu.wordpress.com/faq/")
+  get '/press', to: redirect("https://joatu.wordpress.com/press/")
+  get '/volunteer', to: redirect("https://joatu.wordpress.com/volunteer/")
+  get '/community', to: redirect("https://joatu.wordpress.com/community/")
+  get '/contact', to: redirect("https://joatu.wordpress.com/contact/")
+  get '/blog', to: redirect("http://joatu.wordpress.com/")
+  get '/how-i-thrived-on-a-basic-income-of-11000-last-year-in-montreal', to: redirect("https://joatu.wordpress.com/2015/01/29/how-i-thrived-on-a-basic-income-of-11000-last-year-in-montreal/")
+  get '/grass-to-gardens-joatus-community-initiative', to: redirect("https://joatu.wordpress.com/2014/09/04/grass-to-gardens-joatus-community-initiative/")
+  get '/peoples-social-forum-2014-space-for-change', to: redirect("https://joatu.wordpress.com/2014/08/27/peoples-social-forum-2014-space-for-change/")
+  get '/the-other-side-of-basic-income-basic-jobs', to: redirect("https://joatu.wordpress.com/2014/07/28/the-other-side-of-basic-income-basic-jobs/")
+  get '/what-i-learned-at-the-hopex-hacker-conference', to: redirect("https://joatu.wordpress.com/2014/07/23/what-i-learned-at-the-hopex-hacker-conference/")
+  get '/joatu-relaunches-its-campaign-3500-to-goal', to: redirect("https://joatu.wordpress.com/2014/07/10/joatu-relaunches-its-campaign-3500-to-goal/")
+  get '/press-release-apr15-2014-crowd-funding-campaign-launch', to: redirect("https://joatu.wordpress.com/2014/04/15/press-release-apr15-2014-crowd-funding-campaign-launch/")
 end
