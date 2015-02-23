af4b384ccbd7ce94aee41d729b13da1fb7f68332
Alex Willemsma <alex@undergroundwebdevelopment.com>
2015-02-23 11:18:34 -0800

    Fix bug in pod home page due to null objects.

--- app/models/base.rb
+++ app/models/base.rb
@@ -2,4 +2,8 @@ class Base < ActiveRecord::Base
   self.abstract_class = true
 
   include Paginatable
+
+  NullObject = Naught.build
+  include NullObject::Conversions
+  extend NullObject::Conversions
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -6,10 +6,14 @@ class Event < Base
   composed_of :address, mapping: %w(address_json to_json)
 
   def self.available_for_organization(org, pagination)
+    return none unless Actual(org)
+
     where(organization_id: org.id).paginate(pagination)
   end
 
   def self.available_for_pod(pod, pagination)
+    return none unless Actual(pod)
+
     self.where("organization_id = :org_id OR pod_id = :pod_id", org_id: pod.hub.id, pod_id: pod.id).paginate(pagination)
   end
 end
