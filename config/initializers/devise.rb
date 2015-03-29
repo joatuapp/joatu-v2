cabc6bed6307c6dbe80ab3c748c08f7c24d3a544
Alex Willemsma <alex@undergroundwebdevelopment.com>
2015-03-29 16:58:21 -0700

    Tweak devise initializer to loosen up pw requirements at the model level.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -179,7 +179,9 @@ Devise.setup do |config|
 
   # ==> Configuration for :validatable
   # Range for password length.
-  config.password_length = 8..128
+  #
+  # NOTE: Setting this _very_ permissive, we'll tighten it up with forms:
+  config.password_length = 1..128
 
   # Email regex used to validate email formats. It simply asserts that
   # one (and only one) @ exists in the given string. This is mainly
