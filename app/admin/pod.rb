51911a395f802a12e0da2e60e708c3c585668284
Robert Smith <robert@grenadine.co>
2018-12-06 23:59:05 -0500

    Added Organization to Pods table

--- app/admin/pod.rb
+++ app/admin/pod.rb
@@ -1,10 +1,15 @@
 ActiveAdmin.register Pod do
   permit_params :name, :description, :focus_area, :hub_id
 
+  includes :hub
+
   index do
     selectable_column
     id_column
     column :name
+    column "Organization" do |pod|
+      pod.hub
+    end
     column :created_at
     actions
   end
@@ -15,7 +20,7 @@ ActiveAdmin.register Pod do
     f.inputs "Pod Details" do
       f.input :name
       f.input :description
-      f.select :hub_id, Organization.all.map {|o| [o.name, o.id] }
+      f.input :hub_id, as: :select, collection: Organization.all.select(:id, :name), member_label: :name, label: 'Organization'
       f.input :focus_area, as: :map_area
     end
     f.actions
--- app/admin/user.rb
+++ app/admin/user.rb
@@ -26,7 +26,7 @@ ActiveAdmin.register User do
   filter :created_at
 
   form do |f|
-    f.inputs "Admin Details" do
+    f.inputs "User Details" do
       f.input :email
       f.input :password
       f.input :password_confirmation
