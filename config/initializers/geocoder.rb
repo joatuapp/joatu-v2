eb2bab0f95cd00e7eda706279aeb635921d55fe7
Robert Smith <robert@grenadine.co>
2018-12-15 13:59:36 -0500

    use our api for geocoder

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,12 +1,12 @@
 Geocoder.configure(
   # geocoding options
   # :timeout      => 3,           # geocoding service timeout (secs)
-  # :lookup       => :google,     # name of geocoding service (symbol)
+  :lookup       => :google,     # name of geocoding service (symbol)
   # :language     => :en,         # ISO-639 language code
   :use_https    => true,       # use HTTPS for lookup requests? (if supported)
   # :http_proxy   => nil,         # HTTP proxy server (user:pass@host:port)
   # :https_proxy  => nil,         # HTTPS proxy server (user:pass@host:port)
-  :api_key      => "AIzaSyD2_Z-35nKnF8cayBVShQPNdNzWKgo3Kck",         # API key for geocoding service
+  :api_key      => ENV.fetch("GOOGLE_API_KEY"),         # API key for geocoding service
   # :cache        => nil,         # cache object (must respond to #[], #[]=, and #keys)
   # :cache_prefix => "geocoder:", # prefix (string) to use for all cache keys
 
