239114994e550f5595eee95762258957807ec5a3
Robert Smith <robert@grenadine.co>
2019-01-10 09:21:36 -0500

    added pod and user to offers and requests tables in admin area

--- app/admin/offer.rb
+++ app/admin/offer.rb
@@ -1,10 +1,23 @@
 ActiveAdmin.register Offer do
-  permit_params :title, :description, :created_by_user_id
+  permit_params :title, :description, :user_id
+
+  includes :pod, :user
 
   index do
     selectable_column
     id_column
     column :title
+
+    column "Pod" do |offer|
+      if offer.pod.id
+        link_to offer.pod.name, admin_pod_path(offer.pod)
+      else
+        offer.pod.name
+      end
+    end
+
+    column :user
+
     column :created_at
     actions
   end
@@ -16,7 +29,8 @@ ActiveAdmin.register Offer do
     f.inputs "Offer" do
       f.input :title
       f.input :description
-      f.select :created_by_user_id, User.all.map {|o| [o.name, o.id] }
+      f.input :pod, as: :select, collection: Pod.all.select(:id, :name), member_label: :name
+      f.select :user_id, User.all.map {|o| [o.name, o.id] }
     end
     f.actions
   end
