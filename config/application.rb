10178bc6fb2f37ba955b5c7f9b2703eae08e4429
Robert Smith <robert@grenadine.co>
2019-01-23 13:52:43 -0500

    moved mailer settings from initializer to application.rb
    This will allow them to be overridden per environment

--- config/application.rb
+++ config/application.rb
@@ -45,5 +45,21 @@ module JoatuV2
     # regenerate the schema in Ruby format.
     # config.active_record.schema_format = :sql
     config.active_record.schema_format = :ruby
+
+    config.action_mailer.default_url_options = {
+      host: ENV.fetch('APP_HOST'),
+    }
+    config.action_mailer.default_options = {
+      from: 'Joatu <info@joatu.org>',
+    }
+    config.action_mailer.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => ENV.fetch('APP_HOST'),
+      :enable_starttls_auto => true
+    }
   end
 end
--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,19 +1,19 @@
-module JoatuV2
-  class Application
-    config.action_mailer.default_url_options = {
-      host: ENV.fetch('APP_HOST'),
-    }
-    config.action_mailer.default_options = {
-      from: 'Joatu <info@joatu.org>',
-    }
-    config.action_mailer.smtp_settings = {
-      :address        => 'smtp.sendgrid.net',
-      :port           => '587',
-      :authentication => :plain,
-      :user_name      => ENV['SENDGRID_USERNAME'],
-      :password       => ENV['SENDGRID_PASSWORD'],
-      :domain         => ENV.fetch('APP_HOST'),
-      :enable_starttls_auto => true
-    }
-  end
-end
+# module JoatuV2
+#   class Application
+#     config.action_mailer.default_url_options = {
+#       host: ENV.fetch('APP_HOST'),
+#     }
+#     config.action_mailer.default_options = {
+#       from: 'Joatu <info@joatu.org>',
+#     }
+#     config.action_mailer.smtp_settings = {
+#       :address        => 'smtp.sendgrid.net',
+#       :port           => '587',
+#       :authentication => :plain,
+#       :user_name      => ENV['SENDGRID_USERNAME'],
+#       :password       => ENV['SENDGRID_PASSWORD'],
+#       :domain         => ENV.fetch('APP_HOST'),
+#       :enable_starttls_auto => true
+#     }
+#   end
+# end
